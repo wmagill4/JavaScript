d27fd3d5ef33115a86b67e5c7167dd3463aa2e46
Bill Magill <35304879+wmagill4@users.noreply.github.com>
2018-11-09 17:54:16 -0500

    Upload 3

--- static/js/app.js
+++ static/js/app.js
@@ -3,10 +3,9 @@ var tableData = data;
 
 var rowsPerPage = 150
 
-// *function to reder a given table of data
+// renderTable function
 function renderTable (tData,page)
-// reder table rows under "tbody" class
-// loop through list of objects, each object is one row
+// render the table rows and loop through the objects
 {
     // clean existing table content
     d3.select("tbody").html("")
@@ -24,22 +23,19 @@ function renderTable (tData,page)
     })
 }
 
-// 1. redner the first 20 rows of data when first launch window
+// render all table rows 
 renderTable(tableData.slice(0,rowsPerPage),1);
 
 
-// 2. filter data with form inputs after hit the "filter table" button
+// enable the filter button
 var filteredData = tableData //defualt data if no filter
-
-// Select the submit button
 var filter = d3.select("#filter-btn");
 
 filter.on("click", function() {
-  // Prevent the page from refreshing
+  // Prevent page refresh
   d3.event.preventDefault();
 
-  // Select the input elements and get the values
-    // cleaning - trim spaces and turn into lower case
+  // Select elements, get values, trim to lower case
   var datetime = d3.select("#datetime").property("value");
   datetime = datetime.trim().toLowerCase()
   var city = d3.select("#city").property("value");
@@ -60,10 +56,10 @@ filter.on("click", function() {
       && (tableData.shape.toLowerCase() == shape || shape == "") )
   })
     
-  // render the first 20 result   
+  // render the filtered table
   renderTable(filteredData.slice (0,rowsPerPage),1);
 
-  // if no result returned - give use an alter
+  // if nothing returned, print no result found
   if (filteredData.length===0){
     window.alert("No result found :(");
     }
@@ -82,24 +78,4 @@ d3.select("#next").on("click",function(){
         data = filteredData.slice((page-1)*rowsPerPage,(page-1)*rowsPerPage+rowsPerPage)
         renderTable(data,page)
     }
-})
-// 3.2 when click previous, render the last 20 rows if avaiable
-d3.select("#previous").on("click",function(){
-    var numberOfPages = Math.ceil(filteredData.length/rowsPerPage)
-
-    if (page > 1){
-        page-=1
-        data = filteredData.slice((page-1)*rowsPerPage,(page-1)*rowsPerPage+rowsPerPage)
-        renderTable(data,page)
-        }
-})
-
-// 3.3 when click reset, render the first 20 rows of orignal data
-d3.select("#reset").on("click",function(){
-    //  clean user iputs
-    d3.selectAll(".form-control").property("value","")
-    // render original data
-    page = 1
-    renderTable(tableData.slice(0,rowsPerPage),page);
-    filteredData = tableData; //reset filtered data
 })
